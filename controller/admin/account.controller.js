f8e9f4986c67b55caf136d47a0f9d5da37ed888b
Nguyen Tien Ngoc <ntn322003@gmail.com>
2024-11-23 07:14:43 +0700

    Làm trang sửa tài khoản

--- controller/admin/account.controller.js
+++ controller/admin/account.controller.js
@@ -48,3 +48,47 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${config.prefixAdmin}/accounts`);
   }
 };
+
+module.exports.edit = async (req, res) => {
+  let find = {
+    deleted: false,
+    _id: req.params.id,
+  };
+
+  try {
+    const record = await Account.findOne(find);
+    const roles = await Role.find({
+      deleted: false,
+    });
+    res.render("admin/pages/account/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      record: record,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`${config.prefixAdmin}/accounts`);
+  }
+};
+
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  const emailExist = await Account.findOne({
+    _id: { $ne: id },
+    email: req.body.email,
+    deleted: false,
+  });
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại`);
+  } else {
+    if (req.body.password) {
+      req.body.password = await hashedPassword.hashPassword(req.body.password);
+    } else {
+      delete req.body.password;
+    }
+
+    await Account.updateOne({ _id: id }, req.body);
+
+    req.flash("success", "Cập nhật thành công!");
+  }
+  res.redirect("back");
+};
