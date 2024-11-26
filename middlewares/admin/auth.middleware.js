bf5936b7aaae07dd5a2104f2c2091e25dbef6a15
Nguyen Tien Ngoc <ntn322003@gmail.com>
2024-11-26 10:58:43 +0700

    Thêm Authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,14 +1,22 @@
 const config = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect(`${config.prefixAdmin}/auth/login`);
   } else {
-    const user = await Account.findOne({ token: req.cookies.token });
+    const user = await Account.findOne({ token: req.cookies.token }).select(
+      "-password"
+    );
     if (!user) {
       res.redirect(`${config.prefixAdmin}/auth/login`);
     } else {
+      res.locals.user = user;
+      const role = await Role.findOne({ _id: user.role_id }).select(
+        "title permissions"
+      );
+      res.locals.role = role;
       next();
     }
   }
